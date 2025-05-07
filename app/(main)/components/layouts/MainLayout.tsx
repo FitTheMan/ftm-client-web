0c942d6f41b447eb6953895c071d7689b2c9b7c3
dongmin3891 <rlaehdals753@naver.com>
2025-05-08 00:59:08 +0900

    feat: user-pick 임시 반응형

--- app/(main)/components/layouts/MainLayout.tsx
+++ app/(main)/components/layouts/MainLayout.tsx
@@ -8,26 +8,17 @@ interface MainLayoutProps {
 
 export default function MainLayout({ children }: MainLayoutProps) {
   return (
-    <div className="flex w-full flex-col">
-      {/* 1920px-1504px: 3단 레이아웃 (사이드바 2개 + 메인 콘텐츠) */}
-      {/* 1504px-880px: 1단 레이아웃 (메인 콘텐츠만) */}
-      {/* 880px-544px: 1단 레이아웃 (메인 콘텐츠) */}
-      {/* 544px-375px: 모바일 레이아웃 */}
-      <div className="mx-auto flex w-full max-w-[1920px] justify-between px-4 sm:gap-x-4 md:gap-x-8 lg:gap-x-16 xl:gap-x-24 2xl:gap-x-36">
-        {/* 왼쪽 사이드바 */}
-        <div className="w-[280px] shrink-0 sm:w-[300px] md:w-[324px]">
-          <LeftSidebar />
-        </div>
+    <div className="mx-auto flex w-full max-w-[1920px] justify-center px-4 sm:gap-x-4 md:gap-x-6 lg:justify-between lg:gap-x-10 xl:gap-x-14 2xl:gap-x-20">
+      <div className="hidden w-[280px] shrink-0 sm:w-[300px] md:w-[324px] lg:block">
+        <LeftSidebar />
+      </div>
 
-        {/* 메인 콘텐츠 */}
-        <div className="w-full max-w-[808px] px-2 sm:px-4 md:px-6 lg:px-[45px] 2xl:max-w-[1224px]">
-          {children}
-        </div>
+      <div className="w-full max-w-[808px] px-2 sm:px-4 md:px-6 lg:px-[45px] 2xl:max-w-[1224px]">
+        {children}
+      </div>
 
-        {/* 오른쪽 사이드바 */}
-        <div className="w-[280px] shrink-0 sm:w-[300px] md:w-[324px]">
-          <RightSidebar />
-        </div>
+      <div className="hidden w-[280px] shrink-0 sm:w-[300px] md:w-[324px] lg:block">
+        <RightSidebar />
       </div>
     </div>
   );
--- app/(main)/user-pick/page.tsx
+++ app/(main)/user-pick/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { FiSearch } from "react-icons/fi";
 import Button from "@/components/ui/Button";
 import { useEffect } from "react";
@@ -8,29 +6,23 @@ import { openSigninSelectModal } from "@/utils/modal/OpenSigninSelectModal";
 import SectionHeader from "../components/header/SectionHeader";
 
 export default function UserPick() {
-  useEffect(() => {
-    const user = getUser();
-    if (!user) {
-      openSigninSelectModal();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
     <>
-      <SectionHeader
-        title="유저 Pick"
-        rightContent={
-          <div className="flex items-center space-x-4">
-            <Button variant="icon">
-              <FiSearch className="h-5 w-5 text-gray-600" />
-            </Button>
-            <Button variant="secondary">Filter+</Button>
-          </div>
-        }
-      />
+      <div className="mx-auto w-full max-w-[808px] px-2 sm:px-4 md:px-6 lg:px-[45px] 2xl:max-w-[1224px]">
+        <SectionHeader
+          title="유저 Pick"
+          rightContent={
+            <div className="flex items-center space-x-4">
+              <Button variant="icon">
+                <FiSearch className="h-5 w-5 text-gray-600" />
+              </Button>
+              <Button variant="secondary">Filter+</Button>
+            </div>
+          }
+        />
+      </div>
 
-      <div className="mt-8">
+      <div className="mx-auto mt-8 w-full max-w-[808px] px-2 sm:px-4 md:px-6 lg:px-[45px] 2xl:max-w-[1224px]">
         <h3 className="text-lg font-medium">인기있는 글</h3>
         <p className="mt-1 text-sm text-gray-500">
           지금 사람들이 유저들을 위한 바이블
