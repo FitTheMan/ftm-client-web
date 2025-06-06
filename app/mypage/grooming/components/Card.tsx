6aa2aa27300d23c6f6cd1e619be1a73d698f731f
yu-ratel <dbdudals100@gmail.com>
2025-06-06 20:25:40 +0900

    feat: mypage groomingPage 퍼블리싱

---
+++ app/mypage/grooming/components/Card.tsx
@@ -0,0 +1,12 @@
+const Card = () => {
+  return (
+    <div className="min-h-[358px] w-[calc((100%-24px)/2)] rounded-3xl bg-[#374254]">
+      <div className="m-[10px] h-[280px] rounded-2xl bg-white"></div>
+      <h3 className="py-6 text-center text-2xl font-semibold text-white">
+        날짜
+      </h3>
+    </div>
+  );
+};
+
+export default Card;
--- app/mypage/grooming/page.tsx
+++ app/mypage/grooming/page.tsx
@@ -1,12 +1,24 @@
 import SectionHeader from "../components/SectionHeader";
+import Card from "./components/Card";
 
 const Page = () => {
   return (
-    <SectionHeader
-      title="그루밍 지수 분석"
-      description="검사 결과는 최대 5개까지 등록됩니다."
-    ></SectionHeader>
+    <>
+      <SectionHeader
+        title="그루밍 지수 분석"
+        description="검사 결과는 최대 5개까지 등록됩니다."
+      ></SectionHeader>
+      <div className="flex w-full flex-wrap justify-between gap-6">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </>
   );
 };
 
 export default Page;
+
+// 그루밍 카드 만들었으니 api 로그인 아이디에 저장 후 존재하는 데이터 연결까지 해야함.
