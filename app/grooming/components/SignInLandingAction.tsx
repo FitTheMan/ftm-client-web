3cd17748e067343a6e0c5668bd09492842fd3375
yu-ratel <dbdudals100@gmail.com>
2025-06-06 21:10:59 +0900

    refactor: login user 그루밍지수 검사 여부에 따른 분기처리 추가

--- app/grooming/components/SignInLandingAction.tsx
+++ app/grooming/components/SignInLandingAction.tsx
@@ -1,14 +1,24 @@
+"use client";
+
 import Link from "next/link";
 import { ROUTES } from "@/constants/routes";
+import { UserInfo } from "@/types/user";
 
 const DefaultLandingAction = () => {
+  const userInfo = localStorage.getItem("userInfo") as string;
+  const parsed = JSON.parse(userInfo) as { state: { user: UserInfo } };
+  const user: UserInfo = parsed.state.user;
+  const isFirstGroomingCheck = !!(user.mildLevelName && user.spicyLevelName);
+
   return (
     <section className="flex flex-col gap-4 pr-32 max-xl:pr-0">
       <Link
         href={ROUTES.GROOMING_CHECK}
         className="h-24 w-full content-center self-center rounded-3xl bg-blue-500 px-4 py-2 text-center text-2xl text-white max-md:text-lg"
       >
-        그루밍지수 재검사 하러가기
+        {isFirstGroomingCheck
+          ? "그루밍지수 재검사 하러가기"
+          : "그루밍지수 검사하러가기"}
       </Link>
       <Link
         href={ROUTES.ROOT}
