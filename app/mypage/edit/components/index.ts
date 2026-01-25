fc624679bf18604530837ba1bab297547f51fee4
yu-ratel <dbdudals100@gmail.com>
2026-01-26 00:55:36 +0900

    refactor: mypage infoEdit 수정 api key 값 변경 및 age SeletBox 변경

--- app/mypage/edit/components/index.ts
+++ app/mypage/edit/components/index.ts
@@ -1,2 +1,1 @@
-export * from "./EditInfoBar";
 export * from "./EditInfoButton";
--- app/mypage/edit/components/page/index.tsx
+++ app/mypage/edit/components/page/index.tsx
@@ -1,12 +1,16 @@
 "use client";
 
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 import SectionHeader from "@/app/(main)/components/header/SectionHeader";
 import Button from "@/components/ui/Button";
-import { EditInfoBar, EditInfoButton } from "../";
+import { EditInfoButton } from "../";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { getUserInfo, useUpdateUserInfo } from "../../api";
 import { HashTagInfo } from "../../types";
+import { AGE_OPTIONS } from "../../constants";
+import SelectBox from "@/components/ui/SeletBox";
+import { openToast } from "@/utils/modal/OpenToast";
 
 export default function MyPageEditPage() {
   const { data: userInfoData } = useSuspenseQuery({
@@ -16,19 +20,23 @@ export default function MyPageEditPage() {
   const [selectedInterests, setSelectedInterests] = useState<HashTagInfo[]>(
     userInfoData.hashTagInfo
   );
+  const router = useRouter();
   const { mutate: updateUserInfo } = useUpdateUserInfo();
   const { userNickname, ageInfo } = userInfoData;
-
+  const [selectedAge, setSelectedAge] = useState<string>(ageInfo.description);
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.target as HTMLFormElement);
     const payload = {
       nickname: formData.get("nickname") as string,
-      ageInfo: { value: "FIFIES", description: formData.get("age") as string },
+      ageInfo: {
+        value: AGE_OPTIONS.find((option) => option.description === selectedAge)
+          ?.value,
+        description: selectedAge,
+      },
       hashTagInfo: selectedInterests,
     };
 
-    console.log(payload);
     const requestData = new FormData();
 
     requestData.append(
@@ -36,7 +44,19 @@ export default function MyPageEditPage() {
       new Blob([JSON.stringify(payload)], { type: "application/json" })
     );
 
-    updateUserInfo(requestData);
+    updateUserInfo(requestData, {
+      onSuccess: () => {
+        openToast("회원정보 수정 성공", "정상적으로 수정되었습니다.", 3000);
+        router.back();
+      },
+      onError: () => {
+        openToast(
+          "회원정보 수정 실패",
+          "오류가 발생했습니다. 잠시 후 다시 시도해주세요.",
+          3000
+        );
+      },
+    });
   };
 
   return (
@@ -65,22 +85,30 @@ export default function MyPageEditPage() {
       <section className="text-sm">
         {/* main section */}
         <div className="grid grid-cols-2 gap-x-12">
-          <EditInfoBar
-            title="유저네임*"
-            placeholder={userNickname}
-            name="nickname"
-          />
-          <EditInfoBar
-            title="연령대"
-            placeholder={ageInfo.description}
-            name="age"
-          />
+          <div className="flex flex-col gap-2">
+            <h3 className="font-semibold">유저네임*</h3>
+            <input
+              type="text"
+              placeholder={userNickname}
+              name="nickname"
+              className="w-full rounded-md border border-gray-200 bg-gray-100/50 p-2 focus:outline-none"
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <h3 className="font-semibold">연령대</h3>
+            <SelectBox
+              selectedOption={selectedAge}
+              onSelect={setSelectedAge}
+              placeholder="연령대를 선택해주세요"
+              options={AGE_OPTIONS.map((option) => option.description)}
+            />
+          </div>
         </div>
 
         {/* sub section */}
         <div className="mt-6">
-          <h3>나의 구르밍 관심</h3>
-          <div className="grid grid-cols-2 gap-x-12 gap-y-2">
+          <h3 className="font-semibold">나의 구르밍 관심</h3>
+          <div className="mt-2 grid grid-cols-2 gap-x-12 gap-y-2">
             {selectedInterests?.map((tag) => (
               <EditInfoButton
                 key={tag.value}
@@ -93,7 +121,7 @@ export default function MyPageEditPage() {
       </section>
 
       <section className="flex justify-center text-sm">
-        <Button type="submit" className="w-44 rounded-[10px]">
+        <Button type="submit" className="w-44 rounded-[10px] font-bold">
           저장하기
         </Button>
       </section>
